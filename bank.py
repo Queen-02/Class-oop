5727efa94872bbb5fc10e5396d69deca34f4ef81
Queen-02 <queenzyblack176@gmail.com>
2022-06-09 20:34:38 +0300

    add changes made to bank file

--- bank.py
+++ bank.py
@@ -1,7 +1,31 @@
 class Bank:
     accountType = "Saving Account"
-    def __init__(self, accountNumber, pin, accountName):
+    
+    def __init__(self, accountNumber, accountName):
         self.accountNumber = accountNumber
         self.accountName = accountName
-        self.pin = pin
+        self.balance = 0
+        self.deposits = []
+        self.withdrawals = []
     
+    def deposit(self, amount):
+        if amount <= 0:
+            return f'Deposit amount should be more than zero'
+        else:
+            self.balance += amount
+            self.deposits.append(amount)
+            return f'You have deposited this amount {amount}, and your balance is {self.balance}. {self.deposits}'
+            
+            
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return f'Your balance is {amount}, and you cannot withdraw this {self.balance} amount'
+        elif amount < 0:
+            return f'Amount is negative, and you cannot withdraw'
+        else:
+            self.balance -= amount
+            self.deposit.append(amount)
+            return f'You have withdrawn {amount}, and your balance is {self.balance}. {self.deposit}'
+
+
--- student.py
+++ student.py
@@ -1,16 +1,23 @@
+from re import L
+
+
 class Student:
     # name = 'Kanini'
     # age = 30
     # country = 'Kenya'
     school = 'Akirachix'
-    def __init__(self,name,age,country):
-        self.name = name
+    def __init__(self,firstname, lastname, age,country):
+        self.firstname = firstname
+        self.lastname = lastname
         self.age = age
         self.country = country
     def greet(self):
         return f"Hello {self.name} from {self.country}, welcome to the {self.school}"
-    def otherCredentials(self, firstName, lastName, initials, year_of_birth):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.initials = initials
-        self.year_of_birth = year_of_birth
+    def fullname(self):
+       return self.firstname + " " + self.lastname
+    
+    def year_of_birth(self):
+        return 2022- self.age
+    
+    def initals(self):
+        return self.firstname[0].upper() + self.lastname[0].upper()

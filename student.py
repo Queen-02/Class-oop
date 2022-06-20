c28c2f94b442955f67731a66ac85891469474094
Queen-02 <queenzyblack176@gmail.com>
2022-06-20 14:40:09 +0300

    add changes made to bank file

--- student.py
+++ student.py
@@ -1,23 +1,72 @@
-from re import L
+# from re import L
 
 
-class Student:
-    # name = 'Kanini'
-    # age = 30
-    # country = 'Kenya'
-    school = 'Akirachix'
-    def __init__(self,firstname, lastname, age,country):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.age = age
-        self.country = country
-    def greet(self):
-        return f"Hello {self.name} from {self.country}, welcome to the {self.school}"
-    def fullname(self):
-       return self.firstname + " " + self.lastname
+# class Student:
+#     # name = 'Kanini'
+#     # age = 30
+#     # country = 'Kenya'
+#     school = 'Akirachix'
+#     def __init__(self,firstname, lastname, age,country):
+#         self.firstname = firstname
+#         self.lastname = lastname
+#         self.age = age
+#         self.country = country
+#     def greet(self):
+#         return f"Hello {self.name} from {self.country}, welcome to the {self.school}"
+#     def fullname(self):
+#        return self.firstname + " " + self.lastname
     
-    def year_of_birth(self):
-        return 2022- self.age
+#     def year_of_birth(self):
+#         return 2022- self.age
     
-    def initals(self):
-        return self.firstname[0].upper() + self.lastname[0].upper()
+#     def initals(self):
+#         return self.firstname[0].upper() + self.lastname[0].upper()
+
+
+# def word(text):
+#     if text.reverse() == text:
+#         print ("It's a palindrome word!")
+#     else: 
+#         print ("It's not a palindrome word!")
+# word("madam")
+
+
+
+
+def grades(grade):
+    n = int(input("grade: "))
+    
+
+grades(100)
+
+# nums = {'one':1, 'two':2, 'three':3,'four':4, 'five':5}
+# for i in nums.values():
+#     print (i)
+#     i*i
+
+# nums = range(0, 16)
+# dic = dict()
+# for i in range(0, 16):
+#     dic[i] = i**2
+#     print (i)
+# print (dic)
+
+# # def dic():
+# #     x = {i:i**2 for i in range(0, 15) }
+# #     print (x)
+# # dic() 
+
+# # python program that creates a dictionary with two lists
+# c = [5, 6, 7, 8, 9, 50,65,12]
+# d = [5, 6, 7, 8, 9]
+# e = {}
+# # f = dict(zip(c,d))
+# # print (f)
+
+# for x in d:
+#     for z in c:
+#         e[x] = z
+# print (e)
+
+# # remove duplicates from a list
+# a = [5,6,7,8,9,10,11,12,13,5,5,]
